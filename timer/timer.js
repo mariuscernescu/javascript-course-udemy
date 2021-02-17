f97e5437b78cbb7f052c66242e934bff6fd59228
Mariuszix <ce.c.marius@gmail.com>
2021-02-17 21:39:30 +0200

    daily-update

---
+++ timer/timer.js
@@ -0,0 +1,56 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
+    this.tick();
+    
+    //small improvement that was not in the original practice
+    //this if statement makes sure that you can stop the timer
+    //even if you press multiple times on the play
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    this.interval = setInterval(this.tick, 10);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.01;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
