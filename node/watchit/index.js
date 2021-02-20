9455e250e99166881a6b9ab0472897eee9ab5cbd
Mariuszix <ce.c.marius@gmail.com>
2021-02-20 20:48:24 +0200

    daily-update

---
+++ node/watchit/index.js
@@ -0,0 +1,36 @@
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
+const chalk = require("chalk");
+
+program
+  .version("1.0.0")
+  .argument("[filename]", "Name of a file to execute")
+  .action(async ({ filename }) => {
+    const name = filename || "index.js";
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not fin the file ${name}`);
+    }
+
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.blue(">>>>> Starting process"));
+      proc = spawn("node", [name], { stdio: "inherit" });
+    }, 200);
+
+    chokidar
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
