e7e526d238e0f03f17ca8d9fd646902b90462ee9
Mariuszix <ce.c.marius@gmail.com>
2021-02-27 22:37:23 +0200

    update

--- e-commerce-app/index.js
+++ e-commerce-app/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cookieSession = require('cookie-session');
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users");
+const { response } = require("express");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({
-  keys: ['763yr#eu%ih!73@y2!dyWEF$65etg']
-}));
-app.get("/", (req, res) => {
+app.use(
+  cookieSession({
+    keys: ["763yr#eu%ih!73@y2!dyWEF$65etg"],
+  })
+);
+app.get("/signup", (req, res) => {
   res.send(`
    <div>
+   Your Id is: ${req.session.userId}
        <form method="POST">
           <input name="email" placeholder="email" />
           <input name="password" placeholder="password" />
@@ -22,7 +26,7 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
 
   const existingUser = await usersRepo.getOneBy({ email });
@@ -41,6 +45,41 @@ app.post("/", async (req, res) => {
   req.session.userId = user.id;
 
   res.send("Account created!!");
+});
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("you are logged out");
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`
+  <div>
+  <form method="POST">
+     <input name="email" placeholder="email" />
+     <input name="password" placeholder="password" />
+     <button>Sign In</button>
+  </form>
+</div>
+  `);
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send("Email not found");
+  }
+
+  if (user.password !== password) {
+    return res.send("Invalid password");
+  }
+
+  req.session.userId = user.id;
+
+  res.send("you are signed in!");
 });
 
 app.listen(3000, () => {
